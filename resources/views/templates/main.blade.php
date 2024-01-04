47f20d35cdea706591f874d2e00395cb895442b1
yusufnajhan <najhan.icp@gmail.com>
2024-01-04 11:44:18 +0700

    upload folder project

---
+++ resources/views/templates/main.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>Learning Management System</title>
+    <link rel = "icon" href="https://www.svgrepo.com/show/245681/learning-school.svg" type = "image/x-icon"> 
+</head>
+
+<body>
+    @yield('body')
+</body>
+
+</html>
