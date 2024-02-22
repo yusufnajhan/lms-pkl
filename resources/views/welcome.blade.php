ffb5713ea0455ab22c47aea40c8edf67f3831393
diviass25 <divonschool@gmail.com>
2024-02-22 14:43:05 +0700

    splashh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -56,16 +56,33 @@
     </section>
 </body> --}}
 
-<section class="bg-white dark:bg-gray-900">
-    <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-        <div class="mx-auto max-w-screen-sm text-center">
-            <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">HELLO, YOU!!</h1>
-            <p class="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">Wanna login?</p>
-            <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">please press the button below to go to login page. thanks. </p>
-            <a href="/login" class="inline-flex text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4">Slide to Login Page</a>
-            <a href="/kuis" class="inline-flex text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4">Coba fitur kuis</a>
-        </div>   
-    </div>
+<section class="bg-red dark:bg-red-900">
+    <div class="bg-red">
+        <header class="absolute inset-x-0 top-0 z-50">
+          <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+            <div class="flex lg:flex-1">
+              <a href="/login" class="-m-1.5 p-1.5">
+                <img class="h-8 w-auto" src="https://upload.wikimedia.org/wikipedia/commons/f/f2/Lambang_Kota_Semarang.png" alt="">
+              </a>
+            </div>
+          </nav>
+        </header>
+      
+        <div class="relative isolate px-6 pt-14 lg:px-8">
+          <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+            <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-red-200 to-red-500 opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+          </div>
+        
+          <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+            <div class="text-center">
+              <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Selamat Datang di Learning Management System "SMPedia"</h1>
+              <p class="mt-6 text-lg leading-8 text-gray-600">"Pendidikan Tanpa Batas dengan Learning Management System"</p>
+              <div class="mt-10 flex items-center justify-center gap-x-6">
+                <a href="/login" class="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">Masuk ke LMS<span aria-hidden="true"> →</span></a>
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
 </section>
-
 </html>
